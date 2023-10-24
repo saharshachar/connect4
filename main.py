42d3058e4442d10b705dff3517ad3ee737fa109d
saharshachar <saharshachar@gmail.com>
2023-10-24 22:17:02 +0300

    trying to add victory condition. does not work!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ NUM_OF_PLAYERS = 2  # number of players
 PLAYER_MARKS = [0, 3]
 # value of an empty cell
 EMPTY_CELL_VALUE = -1
+WINNING_STRIKE = 4  # number of pieces in row for winning
 
 
 def creat_board():
@@ -26,6 +27,7 @@ def play_turn(board):
             row_num = get_row_location_for_input(board, col_num)
             return row_num, col_num
 
+
 def is_valid_turn(board, col_num):
     return board[0, col_num] == EMPTY_CELL_VALUE
 
@@ -36,20 +38,67 @@ def get_row_location_for_input(board, col_num):
             return curr_row_index
 
 
-def is_game_over():
-    pass
+def is_game_over(board):
+    for curr_row in range(NUM_ROWS):
+        for curr_col in range(NUM_COLMS):
+            down_combo = check_down_combo(board, curr_row, curr_col, board[curr_row, curr_col])
+            r_daig_combo = check_diag_combo(board, curr_row, curr_col, board[curr_row, curr_col], "right")
+            l_daig_combo = check_diag_combo(board, curr_row, curr_col, board[curr_row, curr_col], "left")
+            # print(f"down {down_combo}, right {r_daig_combo}, left {l_daig_combo}")
+            if down_combo in PLAYER_MARKS:
+                return PLAYER_MARKS.index(down_combo)
+            elif r_daig_combo in PLAYER_MARKS:
+                return PLAYER_MARKS.index(r_daig_combo)
+            elif l_daig_combo in PLAYER_MARKS:
+                return PLAYER_MARKS.index(l_daig_combo)
+    return EMPTY_CELL_VALUE
+
+
+def check_down_combo(board, initial_row, initial_col, curr_player_mark):
+    if not (NUM_ROWS - initial_row >= WINNING_STRIKE):
+        return EMPTY_CELL_VALUE  # no successful down combo
+    else:
+        for curr_row in range(initial_row, (initial_row + WINNING_STRIKE)):
+            if board[curr_row, initial_col] != curr_player_mark:
+                return EMPTY_CELL_VALUE  # no successful down combo
+        return curr_player_mark  # current player has a successful down combo
+
+
+def check_diag_combo(board, initial_row, initial_col, curr_player_mark, right_or_left_diag):
+    if right_or_left_diag == "right":                             # right diagonal check
+        initial_condi = NUM_COLMS - initial_col >= WINNING_STRIKE
+        range_for_loop = range(initial_col, (initial_col + WINNING_STRIKE))
+    else:                                                         # left diagonal check
+        initial_condi = initial_col >= (WINNING_STRIKE - 1)
+        range_for_loop = range((initial_col - WINNING_STRIKE + 1), initial_col)
+
+    loop_index = 0
+    if not (initial_condi and (NUM_ROWS - initial_row >= WINNING_STRIKE)):
+        return EMPTY_CELL_VALUE  # no successful diag combo
+    else:
+        for curr_col in range_for_loop:
+            if board[initial_row + loop_index, curr_col] != curr_player_mark:
+                return EMPTY_CELL_VALUE  # no successful down combo
+            loop_index += 1
+        return curr_player_mark  # current player has a successful down diagonal combo
+
 
 
 if __name__ == '__main__':
     game_over_flag = False
-    board_gama = creat_board()
-    print(board_gama)
+    board_game = creat_board()
+    print(board_game)
     print("\n")
     while not game_over_flag:
 
         for curr_player in range(NUM_OF_PLAYERS):
             print(f"player {curr_player} what is your move?")
-            curr_row_num, curr_col_num = play_turn(board_gama)
-            board_gama[curr_row_num, curr_col_num] = PLAYER_MARKS[curr_player]
-            print(board_gama)
-            # check if victory
+            curr_row_num, curr_col_num = play_turn(board_game)
+            board_game[curr_row_num, curr_col_num] = PLAYER_MARKS[curr_player]
+            print(board_game)
+            winner = is_game_over(board_game)
+            for curr_player_mark in PLAYER_MARKS:
+                if winner == curr_player_mark:
+                    print(f"the winner is player number {PLAYER_MARKS.index(winner)}")
+                    game_over_flag = True
+
